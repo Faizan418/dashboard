51a5391499c5819f1529a5c6b6a945185a2dba4d
Faizan418 <muhammadf4060@gmail.com>
2025-02-11 16:15:37 +0500

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex justify-center  flex-col bg-yellow-400">
+    <h1 className="px-10 py-3 font-bold text-3xl font-serif">Working....</h1> 
+    <h2 className="px-10 py-3 font-bold text-4xl font-serif">Dashboard Comming Soon...</h2> 
+   </div>
+  );
+}
